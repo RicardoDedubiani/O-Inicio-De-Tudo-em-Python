517a8d4e5810caa4916e8a66a158594b5f3774bb
Ricardo Dedubiani <rico.dedubiani@gmail.com>
2017-07-05 23:02:17 -0300

    Create mediaEscolar.PY

---
+++ mediaEscolar.PY
@@ -0,0 +1,4 @@
+nota1 = float(input('Primeira nota: '))
+nota2 = float(input('Segunda nota: '))
+nota3 = float(input('Terceira nota: '))
+print('A média do aluno foi: {:.2f}'.format((nota1+nota2+nota3)/3))
