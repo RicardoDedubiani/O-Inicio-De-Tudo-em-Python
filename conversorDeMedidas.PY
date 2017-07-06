509de67d3eb612c4a18f9eebee0efe5396cc80cc
Ricardo Dedubiani <rico.dedubiani@gmail.com>
2017-07-05 23:24:58 -0300

    Create conversorDeMedidas.PY

---
+++ conversorDeMedidas.PY
@@ -0,0 +1,2 @@
+valor = float(input('Insira um valor em metros: '))
+print('O valor em metros = {}m.\nKilometros = {}km. \nCentimetros = {}cm. \nMilimetros = {}mm'.format(valor, valor/1000, valor*100, valor*1000))
